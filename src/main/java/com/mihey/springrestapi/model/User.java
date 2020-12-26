cf3374cf70ee9f721e0235104f9f5eac7710513f
Mikhail Dolgov <otetc82@yandex.ru>
2020-12-26 16:30:47 +0300

    edit User, edit sql script

--- src/main/java/com/mihey/springrestapi/model/User.java
+++ src/main/java/com/mihey/springrestapi/model/User.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 
 @Setter
 @Getter
@@ -15,6 +16,9 @@ public class User {
     @Id
     @Column(name = "id")
     private Integer id;
+    @Column(name = "user_name")
+    private String userName;
+    @Size(min = 6)
     @Column(name = "password")
     private String password;
     @Enumerated(EnumType.STRING)
