44cc170492bff935025bef398b50e300d48abad8
Mikhail Dolgov <otetc82@yandex.ru>
2020-12-27 15:31:21 +0300

    add all methods to regionController

--- src/main/java/com/mihey/springrestapi/controller/RegionRestControllerV1.java
+++ src/main/java/com/mihey/springrestapi/controller/RegionRestControllerV1.java
@@ -5,8 +5,7 @@ import com.mihey.springrestapi.service.RegionServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,8 +16,31 @@ public class RegionRestControllerV1 {
     private RegionServiceImpl regionService;
 
     @GetMapping("/api/v1/regions")
-    public ResponseEntity<List<Region>> getAllRegions(){
+    public ResponseEntity<List<Region>> getAllRegions() {
         List<Region> regions = regionService.getRegions();
         return new ResponseEntity<>(regions, HttpStatus.OK);
     }
+
+    @GetMapping("/api/v1/regions/{id}")
+    public ResponseEntity<Region> getRegionById(@PathVariable int id) {
+        Region region = regionService.getRegionById(id);
+        return new ResponseEntity<>(region, HttpStatus.OK);
+    }
+
+    @PostMapping("/api/v1/regions")
+    public ResponseEntity<Region> saveRegion(@RequestBody Region region) {
+        return new ResponseEntity<>(regionService.saveRegion(region), HttpStatus.OK);
+    }
+
+    @PutMapping("/api/v1/regions")
+    public ResponseEntity<Region> updateRegion(@RequestBody Region region) {
+        return new ResponseEntity<>(regionService.saveRegion(region), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/api/v1/regions/{id}")
+    public ResponseEntity<String> deleteRegionById(@PathVariable int id) {
+        regionService.deleteRegionById(id);
+        return new ResponseEntity<>("Region successfully deleted", HttpStatus.OK);
+    }
+
 }
