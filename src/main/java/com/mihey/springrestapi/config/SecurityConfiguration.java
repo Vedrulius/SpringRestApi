c59231ac143ce25164342e0fe4ea859936499f39
Mikhail Dolgov <otetc82@yandex.ru>
2020-12-31 01:14:24 +0300

    add jwtFilter, jwtConfigurer

--- src/main/java/com/mihey/springrestapi/config/SecurityConfiguration.java
+++ src/main/java/com/mihey/springrestapi/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.mihey.springrestapi.config;
 import com.mihey.springrestapi.model.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
