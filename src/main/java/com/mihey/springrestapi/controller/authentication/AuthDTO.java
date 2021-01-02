11c466ea1dc2a5e674f2de8dba5a9675d4e069bf
Mikhail Dolgov <otetc82@yandex.ru>
2021-01-02 17:16:30 +0300

    add authController

---
+++ src/main/java/com/mihey/springrestapi/controller/authentication/AuthDTO.java
@@ -0,0 +1,11 @@
+package com.mihey.springrestapi.controller.authentication;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthDTO {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/mihey/springrestapi/controller/authentication/AuthenticationRestControllerV1.java
@@ -0,0 +1,9 @@
+package com.mihey.springrestapi.controller.authentication;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/auth")
+public class AuthenticationRestControllerV1 {
+}
