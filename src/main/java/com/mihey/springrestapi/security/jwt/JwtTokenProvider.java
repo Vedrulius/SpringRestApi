9b69a3615b1a72819fe58a65562b583ba30d9680
Mikhail Dolgov <otetc82@yandex.ru>
2021-01-03 18:49:00 +0300

    add twilio dependency

--- src/main/java/com/mihey/springrestapi/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/mihey/springrestapi/security/jwt/JwtTokenProvider.java
@@ -18,7 +18,7 @@ import java.util.Date;
 public class JwtTokenProvider {
 
 
-    private final UserDetailsService userDetailsService;
+    private UserDetailsService userDetailsService;
 
     @Value("${jwt.secret}")
     private String secretKey;
