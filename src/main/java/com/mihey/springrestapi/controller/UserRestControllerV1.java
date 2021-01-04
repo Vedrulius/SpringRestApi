8b88c3b1446f3f0907e01fcf8a319d507de0cc77
Mikhail Dolgov <otetc82@yandex.ru>
2021-01-04 15:09:33 +0300

    add registerController

--- src/main/java/com/mihey/springrestapi/controller/UserRestControllerV1.java
+++ src/main/java/com/mihey/springrestapi/controller/UserRestControllerV1.java
@@ -1,72 +1,50 @@
 package com.mihey.springrestapi.controller;
 
-import com.mihey.springrestapi.model.Role;
-import com.mihey.springrestapi.model.Status;
 import com.mihey.springrestapi.dto.UserDTO;
 import com.mihey.springrestapi.service.UserServiceImpl;
-import com.mihey.springrestapi.service.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/users")
 public class UserRestControllerV1 {
 
-    private final PasswordEncoder passwordEncoder;
     private final UserServiceImpl userService;
-    private final UserMapper userMapper;
+
 
     @Autowired
-    public UserRestControllerV1(PasswordEncoder passwordEncoder, UserServiceImpl userService, UserMapper userMapper) {
-        this.passwordEncoder = passwordEncoder;
+    public UserRestControllerV1(UserServiceImpl userService) {
         this.userService = userService;
-        this.userMapper = userMapper;
     }
 
-    @PostMapping("/register")
-    public ResponseEntity<UserDTO> addUser(@Valid @RequestBody UserDTO user) {
-        if (userService.findByUserName(userMapper.toEntity(user).getUsername()).isPresent()) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "User exists");
-        }
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        if (user.getStatus() == null) {
-            user.setStatus(Status.CONFIRMATION_REQUIRED);
-        }
-        if (user.getRole() == null) {
-            user.setRole(Role.USER);
-        }
-        return new ResponseEntity<>(userService.saveUser(user), HttpStatus.OK);
-    }
 
-    @GetMapping("/users")
+
+    @GetMapping
     public ResponseEntity<List<UserDTO>> getUsers() {
         UserDetails ud = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return new ResponseEntity<>(userService.getUsers(), HttpStatus.OK);
 
     }
 
-    @GetMapping("/users/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<UserDTO> getUserById(@PathVariable int id) {
         UserDTO user = userService.getUserById(id);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
-    @PutMapping("/users")
+    @PutMapping
     public ResponseEntity<UserDTO> updateUser(@RequestBody UserDTO user) {
         UserDTO u = userService.updateUser(user);
         return new ResponseEntity<>(u, HttpStatus.OK);
     }
 
-    @DeleteMapping("/users/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteUserById(@PathVariable int id) {
         userService.deleteUserById(id);
         return new ResponseEntity<>("User successfully deleted", HttpStatus.OK);
