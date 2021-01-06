716df9fb7e5b4b2c5d1482fe188b68004451d3a9
Mikhail Dolgov <otetc82@yandex.ru>
2021-01-07 00:28:18 +0300

    add code verification

--- src/main/java/com/mihey/springrestapi/controller/authentication/RegisterRestControllerV1.java
+++ src/main/java/com/mihey/springrestapi/controller/authentication/RegisterRestControllerV1.java
@@ -3,19 +3,19 @@ package com.mihey.springrestapi.controller.authentication;
 import com.mihey.springrestapi.dto.UserDTO;
 import com.mihey.springrestapi.model.Code;
 import com.mihey.springrestapi.model.User;
-import com.mihey.springrestapi.repository.UserRepository;
 import com.mihey.springrestapi.service.TwilioVerification;
 import com.mihey.springrestapi.service.UserServiceImpl;
 import com.mihey.springrestapi.service.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -36,9 +36,9 @@ public class RegisterRestControllerV1 {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<UserDTO> addUser(@Valid @RequestBody UserDTO user) {
+    public ResponseEntity<?> addUser(@Valid @RequestBody UserDTO user) {
         if (userService.findByUserName(userMapper.toEntity(user).getUsername()).isPresent()) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "User exists");
+            return new ResponseEntity<>("User exists", HttpStatus.FORBIDDEN);
         }
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         User u = userMapper.toEntity(user);
@@ -46,14 +46,18 @@ public class RegisterRestControllerV1 {
         u.setCode(code);
         code.setUser(u);
         userService.saveUser(u);
-        twilioVerification.sendSms(user.getPhoneNumber(), code);
+        twilioVerification.sendSms(user.getPhoneNumber(), code.getCode());
         return new ResponseEntity<>(userMapper.toDto(u), HttpStatus.OK);
     }
 
     @PostMapping("/verify")
     public ResponseEntity<?> verifyUser(@RequestParam String code, @RequestBody UserDTO user) {
-        User u = userService.findByUserName(userMapper.toEntity(user).getUsername()).orElseThrow(() -> new UsernameNotFoundException(user.getUsername()));
-
-        return new ResponseEntity<>("Invalid code", HttpStatus.BAD_REQUEST);
+        Optional<User> u = userService.findByUserName(userMapper.toEntity(user).getUsername());
+        if (u.isEmpty()){
+            return new ResponseEntity<>("User doesn't exists", HttpStatus.FORBIDDEN);
+        }
+            return twilioVerification.verify(u.get(),code);
     }
+
+
 }
--- src/main/java/com/mihey/springrestapi/service/TwilioVerification.java
+++ src/main/java/com/mihey/springrestapi/service/TwilioVerification.java
@@ -1,35 +1,52 @@
 package com.mihey.springrestapi.service;
 
-import com.mihey.springrestapi.model.Code;
+import com.mihey.springrestapi.model.Status;
+import com.mihey.springrestapi.model.User;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.rest.api.v2010.account.MessageCreator;
 import com.twilio.type.PhoneNumber;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TwilioVerification implements VerificationService {
 
-
-    private final String ACCOUNT_SID;
-
-    private final String AUTH_TOKEN;
-
+    private final UserService userService;
     private final String TWILIO_NUMBER;
+    private final String VALIDATION_TIME;
+
 
     @Autowired
-    public TwilioVerification(@Value("${accountSid}") String ACCOUNT_SID, @Value("${authToken}") String AUTH_TOKEN,
-                        @Value("${twilioNumber}") String TWILIO_NUMBER) {
-        this.ACCOUNT_SID = ACCOUNT_SID;
-        this.AUTH_TOKEN = AUTH_TOKEN;
-        this.TWILIO_NUMBER = TWILIO_NUMBER;
-        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+    public TwilioVerification(@Value("${accountSid}") String accountSid, @Value("${authToken}") String authToken,
+                              UserService userService, @Value("${twilioNumber}") String twilioNumber, @Value("${validationTime}") String validationTime) {
+        this.userService = userService;
+        TWILIO_NUMBER = twilioNumber;
+        VALIDATION_TIME = validationTime;
+        Twilio.init(accountSid, authToken);
     }
 
-    public void sendSms(String phoneNumber, Code code){
-        MessageCreator creator = Message.creator(new PhoneNumber(phoneNumber), new PhoneNumber(TWILIO_NUMBER), code.getCode());
+    public void sendSms(String phoneNumber, String code) {
+        MessageCreator creator = Message.creator(new PhoneNumber(phoneNumber), new PhoneNumber(TWILIO_NUMBER), code);
         creator.create();
+    }
+
+    public ResponseEntity<?> verify(User user, String code) {
+
+        long time = System.currentTimeMillis() - user.getCode().getCreated().getTime();
+        String verifCode = user.getCode().getCode();
+        if (time > Long.parseLong(VALIDATION_TIME)) {
+            return new ResponseEntity<>("Code is expired!", HttpStatus.BAD_REQUEST);
+        }
+        if (!java.util.Objects.equals(code, verifCode)) {
+            return new ResponseEntity<>("Invalid code!", HttpStatus.BAD_REQUEST);
+        } else {
+            user.setStatus(Status.ACTIVE);
+            userService.saveUser(user);
+            return new ResponseEntity<>("Code verified successfully! User status is :" + user.getStatus(), HttpStatus.OK);
+        }
     }
 }
